19b0a0010d0efe94da4338fbe40ab71fcbfbe132
backup1122 <142592568+backup1122@users.noreply.github.com>
2023-11-30 01:05:49 +0600

    Add files via upload

--- index/indexweb.js
+++ index/indexweb.js
@@ -1,9 +1,9 @@
 
 var rotatef = true;
 //var outlink="https://glpy-api.herokuapp.com";
-var outlink="https://gdpy.onrender.com";
+var outlink = "https://gdpy.onrender.com";
 //var outlink='http://127.0.0.1:5000'
-var gitlink='https://raw.githubusercontent.com/backup1122/galleryfiles/master/';
+var gitlink = 'https://raw.githubusercontent.com/backup1122/galleryfiles/master/';
 const token = localStorage.getItem('token');
 const username = 'backup1122';
 const repo = 'galleryfiles';
@@ -16,35 +16,35 @@ function deleteFile(path) {
       'Authorization': `token ${token}`,
     },
   })
-  .then(response => response.json())
-  .then(data => {
-    // Delete the file on GitHub
-    fetch(`https://api.github.com/repos/${username}/${repo}/contents/${path}`, {
-      method: 'DELETE',
-      headers: {
-        'Authorization': `token ${token}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        message: 'Delete file',
-        sha: data.sha,
-      }),
-    })
     .then(response => response.json())
-    .then(deletedFile => {
-      //cout(JSON.parse(this.responseText).done);
-        //snackbar(JSON.parse(this.responseText).done);
-        snackbar("Deleted");
-        
-      console.log('File deleted:', deletedFile);
+    .then(data => {
+      // Delete the file on GitHub
+      fetch(`https://api.github.com/repos/${username}/${repo}/contents/${path}`, {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `token ${token}`,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          message: 'Delete file',
+          sha: data.sha,
+        }),
+      })
+        .then(response => response.json())
+        .then(deletedFile => {
+          //cout(JSON.parse(this.responseText).done);
+          //snackbar(JSON.parse(this.responseText).done);
+          snackbar("Deleted");
+
+          console.log('File deleted:', deletedFile);
+        })
+        .catch(error => {
+          console.error('Error deleting file:', error);
+        });
     })
     .catch(error => {
-      console.error('Error deleting file:', error);
+      console.error('Error fetching file details:', error);
     });
-  })
-  .catch(error => {
-    console.error('Error fetching file details:', error);
-  });
 }
 
 function updateFile(path, updatedBlob) {
@@ -55,41 +55,42 @@ function updateFile(path, updatedBlob) {
       'Authorization': `token ${token}`,
     },
   })
-  .then(response => response.json())
-  .then(data => {
-    // Read the Blob content as a data URL
-    const reader = new FileReader();
-    reader.onloadend = function () {
-      const base64data = reader.result.split(',')[1];
+    .then(response => response.json())
+    .then(data => {
+      // Read the Blob content as a data URL
+      const reader = new FileReader();
+      reader.onloadend = function () {
+        const base64data = reader.result.split(',')[1];
 
-      // Update the file on GitHub
-      fetch(`https://api.github.com/repos/${username}/${repo}/contents/${path}`, {
-        method: 'PUT',
-        headers: {
-          'Authorization': `token ${token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          message: 'Update file',
-          content: base64data,
-          sha: data.sha,
-        }),
-      })
-      .then(response => response.json())
-      .then(updatedFile => {
-        console.log('File updated:', updatedFile);
-        
-      })
-      .catch(error => {
-        console.error('Error updating file:', error);
-      });
-    };
+        // Update the file on GitHub
+        fetch(`https://api.github.com/repos/${username}/${repo}/contents/${path}`, {
+          method: 'PUT',
+          headers: {
+            'Authorization': `token ${token}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            message: 'Update file',
+            content: base64data,
+            sha: data.sha,
+          }),
+        })
+          .then(response => response.json())
+          .then(updatedFile => {
+            snackbar("Updated");
+            console.log('File updated:', updatedFile);
 
-    reader.readAsDataURL(updatedBlob);
-  })
-  .catch(error => {
-    console.error('Error fetching file details:', error);
-  });
+          })
+          .catch(error => {
+            console.error('Error updating file:', error);
+          });
+      };
+
+      reader.readAsDataURL(updatedBlob);
+    })
+    .catch(error => {
+      console.error('Error fetching file details:', error);
+    });
 }
 
 function rotateout(deg = 90) {
@@ -107,7 +108,7 @@ function rotateout(deg = 90) {
 
     console.log(ssr);
     //rotatef = true;
-    rotater(deg,ssr);
+    rotater(deg, ssr);
   }
 }
 
@@ -143,30 +144,43 @@ var addoutdir = () => {
   }
   if (confirm("Are you sure")) {
     cout('y');
-    var ssr = $("#full-image").attr("src");;
+    var anum = parseInt(document.querySelector("#full-image").getAttribute("num"));
+    var ssr = DATA[anum];
     cout(ssr);
     if (ssr.includes('?t=')) {
       ssr = ssr.replace('?t=', ':');
       ssr = /(.+):/.exec(ssr)[1];
     }
     if (document.querySelector("#full-image").src == document.querySelector("body > div.images > img:nth-child(" + now + ")").src) {
-      dell = 1;
+      nowsame = 1;//not changed
     }
     else {
-      dell = 0;
+      nowsame = 0;//changed
     }
-    cout(dell);
-    var anum = parseInt(document.querySelector("#full-image").getAttribute("num"));
-        DATA.splice(anum, 1);
-        document.querySelector("#full-image").src = DATA[anum];
-        if (dell == 1) {
-          document.querySelector("body > div.images > img:nth-child(" + now + ")").src = DATA[anum];
-          //document.querySelector("body > div.images > img:nth-child("+now+")").setAttribute('num',anum)        
-        }
-        if (document.querySelector("#full-image").getAttribute('num') < document.querySelector("body > div.images > img:nth-child(" + now + ")").getAttribute('num')) {
-          document.querySelector("body > div.images > img:nth-child(" + now + ")").setAttribute('num', (document.querySelector("body > div.images > img:nth-child(" + now + ")").getAttribute('num') - 1));
-        }
-    path=ssr.replace(gitlink,'');
+    DATA.splice(anum, 1);
+    if (leftRightTrack == 0) {
+      document.querySelector("#full-image").src = DATA[anum];
+      //console.log('if');
+    }
+    else {
+      leftl();
+      //console.log('else')
+    }
+    if (nowsame == 1) {
+      document.querySelector("body > div.images > img:nth-child(" + now + ")").src = DATA[anum];
+      //document.querySelector("body > div.images > img:nth-child("+now+")").setAttribute('num',anum)        
+    }
+    var mainimages = document.querySelector("body > div.images").childNodes;
+    // if (document.querySelector("#full-image").getAttribute('num') < document.querySelector("body > div.images > img:nth-child(" + now + ")").getAttribute('num')) {//num sync after delete image for re clicking
+    //   document.querySelector("body > div.images > img:nth-child(" + now + ")").setAttribute('num', (document.querySelector("body > div.images > img:nth-child(" + now + ")").getAttribute('num') - 1));
+    // }
+    for (var i = 1; i < mainimages.length; i++) {
+      if (mainimages[i].getAttribute('num') > anum) {
+        mainimages[i].setAttribute('num', (parseInt(mainimages[i].getAttribute('num')) - 1));
+      }
+    }
+
+    path = ssr.replace(gitlink, '');
     deleteFile(path);
     /*var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
@@ -203,7 +217,7 @@ var getoutdir = () => {
       DeleteAll(this.responseText);
     }
   };
-  xhttp.open("GET", outlink+"/getdeletedir", true);
+  xhttp.open("GET", outlink + "/getdeletedir", true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
   xhttp.send();
@@ -211,71 +225,71 @@ var getoutdir = () => {
 }
 var addouthold = () => {
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState === XMLHttpRequest.DONE && this.status == 200) {
-        cout(JSON.parse(this.responseText).done);
-        console.log(JSON.parse(this.responseText).done);
-        snackbar(JSON.parse(this.responseText).done);
-        
-      }
-    };
-    xhttp.open("POST", outlink+"/addhold", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === XMLHttpRequest.DONE && this.status == 200) {
+      cout(JSON.parse(this.responseText).done);
+      console.log(JSON.parse(this.responseText).done);
+      snackbar(JSON.parse(this.responseText).done);
 
-    xhttp.send(JSON.stringify({ hold: JSON.parse(localStorage.getItem('holdlself')) }));
-  
+    }
+  };
+  xhttp.open("POST", outlink + "/addhold", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+
+  xhttp.send(JSON.stringify({ hold: JSON.parse(localStorage.getItem('holdlself')) }));
+
 }
 var delouthold = (a) => {
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState === XMLHttpRequest.DONE && this.status == 200) {
-        cout(JSON.parse(this.responseText).done);
-        console.log(JSON.parse(this.responseText).done);
-        snackbar(JSON.parse(this.responseText).done);
-        
-      }
-    };
-    xhttp.open("POST", outlink+"/delhold", true);
-    xhttp.setRequestHeader("Content-type", "application/json");
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === XMLHttpRequest.DONE && this.status == 200) {
+      cout(JSON.parse(this.responseText).done);
+      console.log(JSON.parse(this.responseText).done);
+      snackbar(JSON.parse(this.responseText).done);
 
-    xhttp.send(JSON.stringify({ hold: a }));
-  
+    }
+  };
+  xhttp.open("POST", outlink + "/delhold", true);
+  xhttp.setRequestHeader("Content-type", "application/json");
+
+  xhttp.send(JSON.stringify({ hold: a }));
+
 }
 function containsObject(obj, list) {
-  var res = _.find(list, function(val){ return _.isEqual(obj, val)});
-  return (_.isObject(res))? true:false;
- }
-var getouthold = (flush=false) => {
-  var holded=JSON.parse(localStorage.getItem('holdl'));
-  if(holded==null){
-    holded=[];
-    localStorage.setItem('holdl',JSON.stringify(holded));
+  var res = _.find(list, function (val) { return _.isEqual(obj, val) });
+  return (_.isObject(res)) ? true : false;
+}
+var getouthold = (flush = false) => {
+  var holded = JSON.parse(localStorage.getItem('holdl'));
+  if (holded == null) {
+    holded = [];
+    localStorage.setItem('holdl', JSON.stringify(holded));
   }
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState === XMLHttpRequest.DONE && this.status == 200) {
       breaker: {
-      cout(this.responseText);
-      if(flush){
-        localStorage.setItem('holdl',this.responseText);
-        break breaker;
-      }
-      var res=JSON.parse(this.responseText);
-      res.forEach(function (obj) {
-        if(!(containsObject(obj,holded))){
-          console.log(obj.now);
-        holded.push(obj);
+        cout(this.responseText);
+        if (flush) {
+          localStorage.setItem('holdl', this.responseText);
+          break breaker;
         }
-      });
-      localStorage.setItem('holdl',JSON.stringify(holded));
+        var res = JSON.parse(this.responseText);
+        res.forEach(function (obj) {
+          if (!(containsObject(obj, holded))) {
+            console.log(obj.now);
+            holded.push(obj);
+          }
+        });
+        localStorage.setItem('holdl', JSON.stringify(holded));
 
-      //DeleteAll(this.responseText);
+        //DeleteAll(this.responseText);
+      }
     }
-  }
   };
-  xhttp.open("GET", outlink+"/gethold", true);
+  xhttp.open("GET", outlink + "/gethold", true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
   xhttp.send();
@@ -290,7 +304,7 @@ var getoutrotatedir = () => {
       RotateAll(this.responseText);
     }
   };
-  xhttp.open("GET", outlink+"/getrotatedir", true);
+  xhttp.open("GET", outlink + "/getrotatedir", true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
   xhttp.send();
@@ -307,7 +321,7 @@ var clearoutdir = () => {
 
     }
   };
-  xhttp.open("GET", outlink+"/cleardelete", true);
+  xhttp.open("GET", outlink + "/cleardelete", true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
   xhttp.send();
@@ -324,7 +338,7 @@ var clearoutrotatedir = () => {
 
     }
   };
-  xhttp.open("GET", outlink+"/clearrotate", true);
+  xhttp.open("GET", outlink + "/clearrotate", true);
   xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
   xhttp.send();
@@ -350,7 +364,7 @@ function rotateweb(srcBase64, degrees, callback) {
 
   image.src = srcBase64;
 }
-var rotater = (deg,src) => {
+var rotater = (deg, src) => {
   rotatef = true;
   const imgr = document.querySelector("#full-image");
   var psrc = imgr.attributes.src.value;
@@ -358,11 +372,11 @@ var rotater = (deg,src) => {
     imgr.setAttribute('src', resultBase64);
     var nsrc = resultBase64;
     if (psrc == document.querySelector("body > div.images > img:nth-child(" + now + ")").src) {
-      document.querySelector("body > div.images > img:nth-child(" + now + ")").src=resultBase64;
+      document.querySelector("body > div.images > img:nth-child(" + now + ")").src = resultBase64;
     }
     DATA[parseInt(imgr.getAttribute('num'))] = resultBase64;
     track_rotate(psrc, nsrc);
-    var path=src.replace(gitlink,'');
+    var path = src.replace(gitlink, '');
     var updatedBlob = dataURItoBlob(resultBase64);
     updateFile(path, updatedBlob);
   });
@@ -385,11 +399,11 @@ var dataURItoBlob = (dataURI) => {
 
   // set the bytes of the buffer to the correct values
   for (var i = 0; i < byteString.length; i++) {
-      ia[i] = byteString.charCodeAt(i);
+    ia[i] = byteString.charCodeAt(i);
   }
 
   // write the ArrayBuffer to a blob, and you're done
-  var blob = new Blob([ab], {type: 'image/jpeg'});
+  var blob = new Blob([ab], { type: 'image/jpeg' });
   return blob;
 
 }
