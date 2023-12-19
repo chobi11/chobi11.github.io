4a872fcc9393b7da46c9c8380c82de928ade0e29
backup1122 <142592568+backup1122@users.noreply.github.com>
2023-12-20 01:22:12 +0600

    Add files via upload

--- index/main.js
+++ index/main.js
@@ -1006,11 +1006,12 @@ function phnopt() {
             + ((big == 1) ? ((web) ? '<tr><td><div><a class="subopt" onclick="rotate(90);hidephnopt();">rotate x1</a> <a class="subopt"  onclick="rotate(180);hidephnopt();">rotate x2</a><a class="subopt"  onclick="rotate(270);hidephnopt();">rotate x3</a></div></td></tr>'
                 : '<tr><td><div><a class="subopt" onclick="rotate(90);hidephnopt();">rotate x1</a> <a class="subopt"  onclick="rotate(180);hidephnopt();">rotate x2</a><a class="subopt"  onclick="rotate(270);hidephnopt();">rotate x3</a></div></td></tr>') : '')
             + ((big == 1) ? `<tr><td><div><a class="subopt" onclick="cropeditfunc('crop');hidephnopt();">Crop</a> &nbsp;&nbsp;<a class="subopt"  onclick="cropeditfunc('edit');hidephnopt();">Edit</a>
-            `+ ((dlist.length != 0 || dblist.length != 0) ? `&nbsp;&nbsp;<a class="subopt"  onclick="UnDelete();hidephnopt();">Undel</a><a class="subopt"  onclick="syncDel();hidephnopt();">dSync</a>` : ``)
+            `+ ((dlist.length != 0 || dblist.length != 0) ? `&nbsp;&nbsp;<a class="subopt"  onclick="UnDelete();hidephnopt();">Undel</a>` : ``)
                 + `</div></td></tr>` : ``)
             +
 
-            ((web) ? `<tr><td><div><a onclick="resetToken();hidephnopt();">Reset token</a></div></td></tr>` : ``)
+            ((web) ? `<tr><td><div><a  class="subopt" onclick="resetToken();hidephnopt();">Reset token</a>&nbsp;&nbsp;
+            <a class="subopt"  onclick="syncDel();hidephnopt();">dSync</a></div></td></tr>` : ``)
             +
             `</table>
         
