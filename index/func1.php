f9706b1e3cd80355170fa4db31c51b5bccbf0c45
chobi11 <showravsbr5@gmail.com>
2024-10-25 18:14:24 +0600

    move

--- index/func1.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $_POST = json_decode(file_get_contents("php://input"),true);
-$fp = fopen('../dir.js', 'w');
-fwrite($fp, 'DATA='.$_POST['DATA'].';'.PHP_EOL .'DATAf='.$_POST['DATAf'].';'.PHP_EOL .'DATAd='.$_POST['DATAd'].';');
-fclose($fp);
-echo "done";
-  ?>
