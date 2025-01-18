eceb3a675bafd1cd9093ddc0353b174690cfe5de
chobi11 <showravsbr5@gmail.com>
2025-01-19 00:40:23 +0600

    Update c_gen_o.py

--- index/script/c_gen_o.py
+++ index/script/c_gen_o.py
@@ -15,8 +15,8 @@ def save(filename, content):
 
 for i in range(len(on)):
     year = on[i]["t"].split("-")[0]
-    month = on[i]["t"].split("-")[1]
-    day = on[i]["t"].split("-")[2]
+    month = on[i]["t"].split("-")[1].lstrip("0")
+    day = on[i]["t"].split("-")[2].lstrip("0")
     k=on[i]["n"]+"_"+str(i)
     date = f"date > datetime({year}, {month}, {day}, 23, 59, 59, 999999) and "
     save(f"{k}.bat", f'gallery-dl --filter "{date}extension not in (\'mp4\', \'gif\', \'m4v\', \'mp3\')" {on[i]["l"]}')
