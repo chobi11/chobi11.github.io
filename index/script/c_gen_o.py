97cf11bf1a5edd7d135e75578141b0f97e1b5432
chobi11 <showravsbr5@gmail.com>
2025-01-19 22:53:00 +0600

    Update c_gen_o.py

--- index/script/c_gen_o.py
+++ index/script/c_gen_o.py
@@ -12,12 +12,21 @@ with open(fileName, 'r') as file:
 def save(filename, content):
     with open(filename, 'w') as file:
         file.write(content)
-
+k=1
+strval=""
 for i in range(len(on)):
+
     year = on[i]["t"].split("-")[0]
     month = on[i]["t"].split("-")[1].lstrip("0")
     day = on[i]["t"].split("-")[2].lstrip("0")
-    k=on[i]["n"]+"_"+str(i)
+    #k=on[i]["n"]+"_"+str(i)
     date = f"date > datetime({year}, {month}, {day}, 23, 59, 59, 999999) and "
-    save(f"{k}.bat", f'gallery-dl --filter "{date}extension not in (\'mp4\', \'gif\', \'m4v\', \'mp3\')" {on[i]["l"]}')
-    print(f"{k}.bat")
+    sv = f"gallery-dl --filter \"{date}extension not in ('mp4', 'gif', 'm4v', 'mp3')\" {on[i]['l']}\n"
+    strval += sv
+    if i != 0 and i % 20 == 0:
+        save(f"{k}.bat", strval)
+        print(f"{k}.bat")
+        strval = ""
+        k += 1
+save(f"{k}.bat", strval)
+print(f"{k}.bat")
